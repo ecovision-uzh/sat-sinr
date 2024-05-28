ae1bdbea16ac85e1d37de489f6ec924867e53b3f
jdollinger-bit <johannes.dollinger@uzh.ch>
2024-05-28 11:10:19 +0200

    added requirements.txt and moves some files.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from embedders import get_embedder
+from models import *
+from dataset import *
+
 import wandb
 import hydra
 import torch
@@ -6,11 +10,9 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
-from embedders import get_embedder
-from models import *
-from dataset import *
 
 def get_logger(params, tag=""):
     """
@@ -23,13 +25,13 @@ def get_logger(params, tag=""):
     elif "sat" in params.model:
         name += " " + params.embedder
     if params.validate:
-        name += " val" 
+        name += " val"
     name += " " + tag
-    
+
     logger = hydra.utils.instantiate({"_target_": "pytorch_lightning.loggers.WandbLogger",
-            "name": name,
-            "save_dir": params.local.logs_dir_path,
-            "project": "sinr_on_glc23"})
+                                      "name": name,
+                                      "save_dir": params.local.logs_dir_path,
+                                      "project": "sinr_on_glc23"})
     return logger
 
 
@@ -38,7 +40,7 @@ def train_model(params, dataset, train_loader, val_loader, provide_model=None, l
     Instantiates model, defines which epoch to save as checkpoint, and trains
     """
     torch.set_float32_matmul_precision('medium')
-    
+
     if not provide_model:
         if params.model == "sinr" or params.model == "log_reg":
             model = SINR(params, dataset)
@@ -46,13 +48,13 @@ def train_model(params, dataset, train_loader, val_loader, provide_model=None, l
             model = SAT_SINR(params, dataset, get_embedder(params))
     else:
         model = provide_model
-    
+
     checkpoint_callback = ModelCheckpoint(
         save_top_k=1,
         monitor="val_loss",
         mode="min",
         dirpath=params.local.cp_dir_path,
-        filename=logger._name+"{val_loss:.4f}"
+        filename=logger._name + "{val_loss:.4f}"
     )
     trainer = pl.Trainer(max_epochs=params.epochs, accelerator=("gpu" if params.local.gpu else "cpu"), devices=1,
                          precision="16-mixed", logger=logger, log_every_n_steps=50,
@@ -68,10 +70,11 @@ def load_cp(params, dataset):
     if params.model == "sinr" or params.model == "log_reg":
         model = SINR.load_from_checkpoint(params.checkpoint, params=params, dataset=dataset)
     elif "sat" in params.model:
-        model = SAT_SINR.load_from_checkpoint(params.checkpoint, params=params, dataset=dataset, sent2_net=get_embedder(params))
+        model = SAT_SINR.load_from_checkpoint(params.checkpoint, params=params, dataset=dataset,
+                                              sent2_net=get_embedder(params))
     return model
 
-        
+
 @hydra.main(version_base=None, config_path='config', config_name='base_config.yaml')
 def main(params):
     """main funct"""
@@ -79,11 +82,12 @@ def main(params):
     logger = get_logger(params, tag=params.tag)
     if params.checkpoint != "None":
         model = load_cp(params, dataset)
-        model = train_model(params, dataset, train_loader, val_loader, provide_model=model, logger=logger, validate=params.validate)
+        model = train_model(params, dataset, train_loader, val_loader, provide_model=model, logger=logger,
+                            validate=params.validate)
     else:
         model = train_model(params, dataset, train_loader, val_loader, logger=logger)
     wandb.finish()
 
-    
+
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -6,14 +6,15 @@ import os
 import pandas as pd
 import numpy as np
 import pytorch_lightning as pl
-#from utils import RandomEuropeDS
 from sklearn.metrics import roc_auc_score
+
 
 class ResidLayer(torch.nn.Module):
     """Residual block used in SINR_Net"""
+
     def __init__(self, hidden_dim, dropout):
         super().__init__()
-        
+
         self.layers = torch.nn.Sequential(
             torch.nn.Linear(hidden_dim, hidden_dim),
             torch.nn.ReLU(),
@@ -21,25 +22,27 @@ class ResidLayer(torch.nn.Module):
             torch.nn.Linear(hidden_dim, hidden_dim),
             torch.nn.ReLU()
         )
-        
+
     def forward(self, x):
         b = self.layers(x)
         x = x + b
         return x
 
+
 class SINR_Net(torch.nn.Module):
     """Base SINR net"""
-    def __init__(self, input_len=4, hidden_dim = 256, dropout = 0.5, layers = 4):
+
+    def __init__(self, input_len=4, hidden_dim=256, dropout=0.5, layers=4):
         super().__init__()
-        
+
         self.location_encoder = torch.nn.Sequential(
             torch.nn.Linear(input_len, hidden_dim),
             torch.nn.ReLU(),
-            *[ResidLayer(hidden_dim, dropout) for i in range(layers)]
+            *[ResidLayer(hidden_dim, dropout) for _ in range(layers)]
         )
-        
+
         self.classifier = self.net = torch.nn.Linear(hidden_dim, 10040)
-        
+
     def forward(self, x):
         x = self.location_encoder(x)
         x = self.classifier(x)
@@ -48,30 +51,28 @@ class SINR_Net(torch.nn.Module):
 
 class SINR(pl.LightningModule):
     """Base SINR, including metric calculations used in all models"""
+
     def __init__(self, params, dataset, **kwargs):
         super().__init__(**kwargs)
-        
+
         input_len = 0
         if "loc" in params.dataset.predictors:
             input_len += 4
         if "env" in params.dataset.predictors:
             input_len += 20
-        
+
         self.params = params
         self.predictors = params.dataset.predictors
         if params.model == "log_reg":
             self.net = torch.nn.Linear(input_len, 10040)
         elif params.model == "sinr":
-            self.net = SINR_Net(input_len, hidden_dim = params.sinr_hidden, dropout = params.dropout, layers = params.sinr_layers)
+            self.net = SINR_Net(input_len, hidden_dim=params.sinr_hidden, dropout=params.dropout,
+                                layers=params.sinr_layers)
         self.dataset = dataset
         self.val_data = dataset.val_data
-        self.cutoff = 0.5 
-        
-        """if "env" in params.dataset.predictors:
-            context_feats = dataset.raster"""
-        
+
         self.save_hyperparameters(ignore=['sent2_net', 'dataset', 'random_europe_ds'])
-        
+
     def forward(self, x):
         # Very unclean, but taking care of other predictor combinations in forward, not dataset
         # TODO: Make clean
@@ -87,11 +88,11 @@ class SINR(pl.LightningModule):
             elif not "loc" in self.predictors:
                 enc = x[:, 4:]
         return self.net(enc)
-    
+
     def apply_model_and_an_full_loss(self, batch, dataset, params):
         loc_features, labels = batch
         random_loc_features = dataset.sample_encoded_locs(len(batch)).to(labels.device)
-        
+
         loc_pred = torch.sigmoid(self(loc_features))
         rand_pred = torch.sigmoid(self(random_loc_features))
 
@@ -109,7 +110,7 @@ class SINR(pl.LightningModule):
         log_dict = {"train_loss": loss_detached}
         self.log_dict(log_dict, batch_size=len(batch))
         return {"loss": loss, "progress_bar": float(loss_detached)}
-    
+
     def _get_pred_from_key(self, key, device):
         splt_key = key.split("/")
         lon = float(splt_key[0])
@@ -120,16 +121,20 @@ class SINR(pl.LightningModule):
         else:
             enc = x
         return self.net(enc)
-    
+
     def _calculate_val_metrics(self, log_dict, device):
-        
+
         if self.params.model.startswith("sat_sinr_mf_"):
             for i in range(len(self.net.net.resid_l)):
-                log_dict["embedder_stats/" + str(i) + "_" + "weight_mean"] = self.net.net.resid_l[i].embedder.weight.abs().mean()
-                log_dict["embedder_stats/" + str(i) + "_" + "weight_std"] = self.net.net.resid_l[i].embedder.weight.abs().std()
-                log_dict["embedder_stats/" + str(i) + "_" + "bias_mean"] = self.net.net.resid_l[i].embedder.bias.abs().mean()
-                log_dict["embedder_stats/" + str(i) + "_" + "bias_std"] = self.net.net.resid_l[i].embedder.bias.abs().std()
-        
+                log_dict["embedder_stats/" + str(i) + "_" + "weight_mean"] = self.net.net.resid_l[
+                    i].embedder.weight.abs().mean()
+                log_dict["embedder_stats/" + str(i) + "_" + "weight_std"] = self.net.net.resid_l[
+                    i].embedder.weight.abs().std()
+                log_dict["embedder_stats/" + str(i) + "_" + "bias_mean"] = self.net.net.resid_l[
+                    i].embedder.bias.abs().mean()
+                log_dict["embedder_stats/" + str(i) + "_" + "bias_std"] = self.net.net.resid_l[
+                    i].embedder.bias.abs().std()
+
         micro_f1_cutoff = 0
         micro_f1_top20 = 0
         micro_f1_top30 = 0
@@ -153,13 +158,13 @@ class SINR(pl.LightningModule):
             if TP > 0:
                 micro_f1_top30 += TP / (TP + (FP + FN) / 2)
             else:
-                micro_f1_top30 += 0 
+                micro_f1_top30 += 0
 
             """top_30 = torch.zeros(10040, dtype=torch.bfloat16).to(device)
             top_30[indics] = pred[indics]"""
             preds.append(pred)
             labels.append(occs)
-                
+
         preds = torch.stack(preds).to("cpu")
         labels = torch.stack(labels).to("cpu")
         sums = labels.sum(dim=0) != 0
@@ -167,48 +172,50 @@ class SINR(pl.LightningModule):
         preds = preds[:, sums]
         log_dict["roc_auc_score_macro"] = roc_auc_score(labels, preds.type(torch.float), average="macro")
         log_dict["roc_auc_score_weighted"] = roc_auc_score(labels, preds.type(torch.float), average="weighted")
-        
-        log_dict["micro_f1_cutoff"] = micro_f1_cutoff/len(self.val_data)
-        log_dict["micro_f1_top30"] = micro_f1_top30/len(self.val_data)
-        log_dict["predicted_present_cutoff"] = PP_cutoff/len(self.val_data)
-        
+
+        log_dict["micro_f1_cutoff"] = micro_f1_cutoff / len(self.val_data)
+        log_dict["micro_f1_top30"] = micro_f1_top30 / len(self.val_data)
+        log_dict["predicted_present_cutoff"] = PP_cutoff / len(self.val_data)
+
         return log_dict
-    
+
     def validation_step(self, batch, batch_nb):
         device = batch[1].device
-        
+
         loss = self.apply_model_and_an_full_loss(batch, self.dataset, self.params)
         loss_detached = loss.detach().cpu()
         log_dict = {"val_loss": loss_detached}
-        
+
         if batch_nb == 0:
             self._calculate_val_metrics(log_dict, device)
-                   
+
         self.log_dict(log_dict, batch_size=len(batch))
         return {"loss": loss, "progress_bar": float(loss_detached)}
-    
+
     def configure_optimizers(self):
         opt = torch.optim.Adam(self.net.parameters(), lr=self.params.lr,
-                              weight_decay = self.params.l2_dec)
+                               weight_decay=self.params.l2_dec)
         return opt
 
-    
+
 class SAT_SINR(SINR):
     """Abstract Sat-Sinr with adapted loss """
+
     def __init__(self, params, dataset, sent2_net, **kwargs):
         super().__init__(params, dataset, **kwargs)
         self.net = get_model(params, params.model)
         self.net.sent2_net = sent2_net
         self.dataset = dataset
         self.mode = None
-        
+
         if params.dataset.use_ds_samples:
             reds = dataset
         else:
             reds = RandomEuropeDS(dataset, params.local.bioclim_path, use_ds_samples=False)
-        self.reds = torch.utils.data.DataLoader(reds, shuffle=True, batch_size=params.dataset.batchsize, num_workers=params.dataset.num_workers)
+        self.reds = torch.utils.data.DataLoader(reds, shuffle=True, batch_size=params.dataset.batchsize,
+                                                num_workers=params.dataset.num_workers)
         self.random_europe_ds = iter(self.reds)
-    
+
     def set_mode(self, mode=None):
         if mode == "sat_only":
             for n, p in model.net.named_parameters():
@@ -223,19 +230,21 @@ class SAT_SINR(SINR):
         else:
             for n, p in model.net.named_parameters():
                 p.requires_grad = True
-    
+
     def apply_model_and_an_full_loss(self, batch, dataset, params):
         loc_features, sent2_images, labels = batch
-        #random_loc_features = dataset.sample_encoded_locs(len(batch)).to(labels.device)
-        #rand_pred = torch.sigmoid(self.net(random_loc_features))
+        # random_loc_features = dataset.sample_encoded_locs(len(batch)).to(labels.device)
+        # rand_pred = torch.sigmoid(self.net(random_loc_features))
         try:
             random_loc_features, random_sent2, _ = next(self.random_europe_ds)
         except:
             self.random_europe_ds = iter(self.reds)
             random_loc_features, random_sent2, _ = next(self.random_europe_ds)
-            
-        rand_pred = torch.sigmoid(self.net((random_loc_features.to(loc_features.device), random_sent2.to(loc_features.device)), no_sent2=False))
-        loc_pred = torch.sigmoid(self.net((loc_features, sent2_images), no_sent2 = False))
+
+        rand_pred = torch.sigmoid(
+            self.net((random_loc_features.to(loc_features.device), random_sent2.to(loc_features.device)),
+                     no_sent2=False))
+        loc_pred = torch.sigmoid(self.net((loc_features, sent2_images), no_sent2=False))
 
         inds = torch.arange(len(labels))
 
@@ -244,7 +253,7 @@ class SAT_SINR(SINR):
         loss_pos[inds, labels] = params.pos_weight * -torch.log(loss_pos[inds, labels] + 1e-5)
 
         return loss_pos.mean() + loss_bg.mean()
-    
+
     def _get_pred_from_key(self, key, device):
         splt_key = key.split("/")
         lon = float(splt_key[0])
@@ -253,16 +262,17 @@ class SAT_SINR(SINR):
         sent2 = self.dataset.get_gbif_sent2(patchID).to(device)
         return self.net((self.dataset.encode(lon, lat).to(device), sent2), no_sent2=False)
 
-    
+
 class SASI_LF(torch.nn.Module):
     """Late fusion Sat-SINR that can also be used as sat-only"""
+
     def __init__(self, params, sat_only=False):
         super().__init__()
-        self.net = SINR_Net(24, hidden_dim = params.sinr_hidden, dropout = params.dropout, layers = params.sinr_layers)
+        self.net = SINR_Net(24, hidden_dim=params.sinr_hidden, dropout=params.dropout, layers=params.sinr_layers)
         self.sent2_to_classes = torch.nn.Linear(256, 10040)
         self.sat_only = sat_only
-        
-    def forward(self, x, no_sent2 = True):
+
+    def forward(self, x, no_sent2=True):
         if no_sent2:
             return self.net(x)
         enc, sent2 = x
@@ -277,9 +287,10 @@ class SASI_LF(torch.nn.Module):
 
 class ContextResidLayer(torch.nn.Module):
     """Residual layer including context information for middle fusion with various initializations for the merging layer"""
+
     def __init__(self, hidden_dim, dropout, mode=""):
         super().__init__()
-        
+
         self.layers = torch.nn.Sequential(
             torch.nn.Linear(hidden_dim, hidden_dim),
             torch.nn.ReLU(),
@@ -310,10 +321,10 @@ class ContextResidLayer(torch.nn.Module):
                 torch.nn.init.kaiming_uniform_(self.embedder.weight)
                 self.embedder.bias.detach().zero_()
         elif mode == "attn":
-            self.embedder = torch.nn.MultiheadAttention(hidden_dim,1)
+            self.embedder = torch.nn.MultiheadAttention(hidden_dim, 1)
         else:
             self.embedder = torch.nn.Linear(2 * hidden_dim, hidden_dim)
-        
+
     def forward(self, x, c=None):
         if c is None:
             if self.layers[0].bias.device != x.device:
@@ -342,18 +353,19 @@ class ContextResidLayer(torch.nn.Module):
             x = x + c_emb
         return x
 
-    
+
 class Context_SINR_Net(torch.nn.Module):
     """Sinr but using context vector"""
-    def __init__(self, input_len=4, hidden_dim = 256, dropout = 0.5, layers = 4, mode=""):
+
+    def __init__(self, input_len=4, hidden_dim=256, dropout=0.5, layers=4, mode=""):
         super().__init__()
-        
+
         self.inp_l = torch.nn.Linear(input_len, hidden_dim)
         self.relu = torch.nn.ReLU()
         self.resid_l = torch.nn.Sequential(*[ContextResidLayer(hidden_dim, dropout, mode) for i in range(layers)])
-        
+
         self.classifier = self.net = torch.nn.Linear(hidden_dim, 10040)
-        
+
     def forward(self, x, c=None):
         x = self.inp_l(x)
         x = self.relu(x)
@@ -362,8 +374,10 @@ class Context_SINR_Net(torch.nn.Module):
         x = self.classifier(x)
         return x
 
+
 class SASI_MF(torch.nn.Module):
     """Only SINR adaption to not use SINR_Net. Has various options use different parameter combinations and encoding mechanisms"""
+
     def __init__(self, params, mode=""):
         super().__init__()
         # nearly all special combinations of predictors outside of the base 3 and sat only is handled here through dataset.predictors
@@ -372,17 +386,18 @@ class SASI_MF(torch.nn.Module):
             inp_size += 4
         if "env" in params.dataset.predictors:
             inp_size += 20
-        self.net = Context_SINR_Net(inp_size, hidden_dim = params.sinr_hidden, dropout = params.dropout, layers = params.sinr_layers, mode=mode)
+        self.net = Context_SINR_Net(inp_size, hidden_dim=params.sinr_hidden, dropout=params.dropout,
+                                    layers=params.sinr_layers, mode=mode)
         self.predictors = params.dataset.predictors
-        
-    def forward(self, x, no_sent2 = True):
+
+    def forward(self, x, no_sent2=True):
         if no_sent2:
             if not "env" in self.predictors:
                 enc = x[:4]
             elif not "loc" in self.predictors:
                 enc = x[4:]
             return self.net(enc)
-        
+
         enc, sent2 = x
         if len(enc.shape) == 1:
             if not "env" in self.predictors:
@@ -391,60 +406,64 @@ class SASI_MF(torch.nn.Module):
                 enc = enc[4:]
         else:
             if not "env" in self.predictors:
-                enc = enc[:,:4]
+                enc = enc[:, :4]
             elif not "loc" in self.predictors:
-                enc = enc[:,4:]
+                enc = enc[:, 4:]
         if no_sent2:
             return self.net(enc)
         sent2_enc = self.sent2_net(sent2)
         if sent2_enc.shape[0] == 1:
             sent2_enc = sent2_enc.view(sent2_enc.shape[1])
         return self.net(enc, c=sent2_enc)
-    
+
+
 class SASI_EF(torch.nn.Module):
     """Early fusion Sat-SINR. The most straightforward of the three."""
+
     def __init__(self, params, enc_dim=24):
         super().__init__()
         self.enc_dim = enc_dim
-        self.net = SINR_Net(24 + enc_dim, hidden_dim = params.sinr_hidden, dropout = params.dropout, layers = params.sinr_layers)
+        self.net = SINR_Net(24 + enc_dim, hidden_dim=params.sinr_hidden, dropout=params.dropout,
+                            layers=params.sinr_layers)
         self.sent2_to_input = torch.nn.Linear(256, enc_dim)
-        
-    def forward(self, x, no_sent2 = True):
+
+    def forward(self, x, no_sent2=True):
         if no_sent2:
-            return self.net(torch.cat((x, torch.zeros(x.shape[0],self.enc_dim).to(x.device)), dim=-1))
+            return self.net(torch.cat((x, torch.zeros(x.shape[0], self.enc_dim).to(x.device)), dim=-1))
         enc, sent2 = x
         sent2_enc = self.sent2_net(sent2)
         sent_input = self.sent2_to_input(sent2_enc)
         if sent_input.shape[0] == 1:
             sent_input = sent_input.view(sent_input.shape[1])
         return self.net(torch.cat((enc, sent_input), dim=-1))
-    
+
+
 def get_model(params, model):
-        """Function to get model based on parameter"""
-        if model == "sat_sinr_lf":
-            net = SASI_LF(params)
-        elif model == "sat_only":
-            net = SASI_LF(params, sat_only=True)
-        elif model == "sat_sinr_ef":
-            net = SASI_EF(params)
-        elif model == "sat_sinr_mf":
-            net = SASI_MF(params)
-        elif model == "sat_sinr_mf_zc":
-            net = SASI_MF(params, mode="zero_conv_zero")
-        elif model == "sat_sinr_mf_zc_n":
-            net = SASI_MF(params, mode="zero_conv_normal")
-        elif model == "sat_sinr_mf_zc_xu":
-            net = SASI_MF(params, mode="zero_conv_xavier_uni")
-        elif model == "sat_sinr_mf_zc_xn":
-            net = SASI_MF(params, mode="zero_conv_xavier_normal")
-        elif model == "sat_sinr_mf_zc_kn":
-            net = SASI_MF(params, mode="zero_conv_kaiming_normal")
-        elif model == "sat_sinr_mf_zc_ku":
-            net = SASI_MF(params, mode="zero_conv_kaiming_uni")
-        elif model == "sat_sinr_mf_zc_u":
-            net = SASI_MF(params, mode="zero_conv")
-        elif model == "sat_sinr_mf_attn":
-            net = SASI_MF(params, mode="attn")
-        else:
-            raise NotImplementeError
-        return net
+    """Function to get model based on parameter"""
+    if model == "sat_sinr_lf":
+        net = SASI_LF(params)
+    elif model == "sat_only":
+        net = SASI_LF(params, sat_only=True)
+    elif model == "sat_sinr_ef":
+        net = SASI_EF(params)
+    elif model == "sat_sinr_mf":
+        net = SASI_MF(params)
+    elif model == "sat_sinr_mf_zc":
+        net = SASI_MF(params, mode="zero_conv_zero")
+    elif model == "sat_sinr_mf_zc_n":
+        net = SASI_MF(params, mode="zero_conv_normal")
+    elif model == "sat_sinr_mf_zc_xu":
+        net = SASI_MF(params, mode="zero_conv_xavier_uni")
+    elif model == "sat_sinr_mf_zc_xn":
+        net = SASI_MF(params, mode="zero_conv_xavier_normal")
+    elif model == "sat_sinr_mf_zc_kn":
+        net = SASI_MF(params, mode="zero_conv_kaiming_normal")
+    elif model == "sat_sinr_mf_zc_ku":
+        net = SASI_MF(params, mode="zero_conv_kaiming_uni")
+    elif model == "sat_sinr_mf_zc_u":
+        net = SASI_MF(params, mode="zero_conv")
+    elif model == "sat_sinr_mf_attn":
+        net = SASI_MF(params, mode="attn")
+    else:
+        raise NotImplementeError
+    return net
